eb9f7bad0c9814d5670a873d0db1f563ac1c6d3d
Rasanhii <jonataspereira788@gmail.com>
2024-10-16 15:44:05 +0000

    Atualizando imagem do header

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,9 +1,10 @@
 import './navbar.css';
+import gamtHeader from '../../assets/gamt-header.png'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="gamt-cursos/src/assets/gamt-header.png" alt="Gamt Header" className="navbar-logo" />
+      <img src={gamtHeader} alt="Gamt Header" className="navbar-logo" />
       <div className="nav-links-container">
         <ul className="nav-links">
           <li><a href="/">Cursos</a></li>
