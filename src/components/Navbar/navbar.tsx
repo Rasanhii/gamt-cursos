0628077ea6ac10c86d5661fe8c22517382ff2177
Rasanhii <jonataspereira788@gmail.com>
2024-10-23 22:21:49 +0000

    Redirecionamento de página

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <img src={gamtHeader} alt="Gamt Header" className="navbar-logo" />
       <div className="nav-links-container">
         <ul className="nav-links">
-          <li><a href="/">Cursos</a></li>
+          <li><a href="/gamt-cursos">Cursos</a></li>
           <li><a href="/gamt-cursos/sobre">Sobre</a></li>
           <li><a href="/gamt-cursos/login">Login</a></li>
         </ul>
