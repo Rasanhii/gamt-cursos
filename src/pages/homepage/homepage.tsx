1edcdb6d9d02032c9b398d4e5ebb6655165a19b3
Rasanhii <jonataspereira788@gmail.com>
2024-10-30 15:27:08 +0000

    Adicionando curso powerpoint

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,5 +1,7 @@
 import './homepage.css';
-import excelImage from '../../assets/excel-avancado-1.jpg';
+import excelImage from '../../assets/excel.jpg';
+import wordImage from '../../assets/word3.jpg';
+import powerImage from '../../assets/powerpoint4.jpg';
 import { useNavigate } from 'react-router-dom'; // Importa o hook para navegação
 
 const Home = () => {
@@ -16,6 +18,7 @@ const Home = () => {
       </div>
       <div className="main-content">
         <div className="courses-container">
+
           <div className="curso" onClick={handleExcelClick}>
             <img src={excelImage} alt="Curso de Excel Avançado" className="curso-image" />
             <div className="curso-info">
@@ -23,10 +26,23 @@ const Home = () => {
               <span>Aprenda Excel do básico ao avançado com exercícios práticos.</span>
             </div>
           </div>
+
           <div className="curso">
-            <p>Curso de Node.js</p>
-            <span>Dominando o backend com Node.js.</span>
+            <img src={wordImage} alt="Curso de Word" className="curso-image" />
+            <div className="curso-info">
+              <p>Curso de Word Avançado</p>
+              <span>Domine as funções e ferramentas do Word aqui.</span>
+            </div>
           </div>
+
+          <div className="curso" onClick={handleExcelClick}>
+            <img src={powerImage} alt="Curso de Powerpoint Avançado" className="curso-image" />
+            <div className="curso-info">
+              <p>Curso de Powerpoint Avançado</p>
+              <span>Desenvolva suas habilidades em confeccionar apresentações.</span>
+            </div>
+          </div>
+          
           <div className="curso">
             <p>Curso de TypeScript</p>
             <span>Tipos no JavaScript para maior segurança.</span>
