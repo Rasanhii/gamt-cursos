7c25bf774fd51e5e5ac6e1cc94e081cb2e303b17
Rasanhii <jonataspereira788@gmail.com>
2024-11-28 18:45:39 +0000

    Ajuste para mobile

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './homepage.css';
 import excelImage from '../../assets/excel.jpg';
 import wordImage from '../../assets/word3.jpg';
@@ -5,7 +6,7 @@ import powerImage from '../../assets/powerpoint4.jpg';
 import powerbiImage from '../../assets/powerbi2.png';
 import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+const Home: React.FC = () => {
   const navigate = useNavigate();
 
   const handleExcelClick = () => {
@@ -13,12 +14,12 @@ const Home = () => {
   };
 
   return (
-    <div className="homepage-container">
-
-      
-
-      <div className="main-content">
+    <div className="homepage">
+      <div className="homepage-container">
+        <h1>Cursos Online</h1>
+        <p>Aprenda habilidades essenciais para o mercado de trabalho.</p>
         <div className="courses-container">
+          {/* Card 1 */}
           <div className="curso" onClick={handleExcelClick}>
             <img src={excelImage} alt="Curso de Excel Avançado" className="curso-image" />
             <div className="curso-info">
@@ -26,7 +27,7 @@ const Home = () => {
               <span>Aprenda Excel do básico ao avançado com exercícios práticos.</span>
             </div>
           </div>
-
+          {/* Card 2 */}
           <div className="curso">
             <img src={wordImage} alt="Curso de Word" className="curso-image" />
             <div className="curso-info">
@@ -34,16 +35,16 @@ const Home = () => {
               <span>Domine as funções e ferramentas do Word aqui.</span>
             </div>
           </div>
-
-          <div className="curso" onClick={handleExcelClick}>
+          {/* Card 3 */}
+          <div className="curso">
             <img src={powerImage} alt="Curso de Powerpoint Avançado" className="curso-image" />
             <div className="curso-info">
-              <p>Curso de Powerpoint Avançado</p>
+              <p>Curso de PowerPoint Avançado</p>
               <span>Desenvolva suas habilidades em confeccionar apresentações.</span>
             </div>
           </div>
-
-          <div className="curso" onClick={handleExcelClick}>
+          {/* Card 4 */}
+          <div className="curso">
             <img src={powerbiImage} alt="Curso de Power BI Avançado" className="curso-image" />
             <div className="curso-info">
               <p>Curso de Power BI</p>
